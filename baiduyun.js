24e6417e472ca1c6ba3662282144ff7d832f2673
rxwy001 <109052991+rxwy001@users.noreply.github.com>
2022-07-23 13:09:34 +0800

    Update baiduyun.js

--- baiduyun.js
+++ baiduyun.js
@@ -1,63 +1,36 @@
-const cookieName = '美团'
-const tokenurlKey = 'chavy_tokenurl_meituan'
-const tokenheaderKey = 'chavy_tokenheader_meituan'
-const signurlKey = 'chavy_signurl_meituan'
-const signheaderKey = 'chavy_signheader_meituan'
-const signbodyKey = 'chavy_signbody_meituan'
-const chavy = init()
-
-const requrl = $request.url
-if ($request && $request.method != 'OPTIONS' && requrl.match(/\/evolve\/signin\/signpost\//)) {
-  const signurlVal = requrl
-  const signheaderVal = JSON.stringify($request.headers)
-  const signbodyVal = $request.body
-  if (signurlVal) chavy.setdata(signurlVal, signurlKey)
-  if (signheaderVal) chavy.setdata(signheaderVal, signheaderKey)
-  if (signbodyVal) chavy.setdata(signbodyVal, signbodyKey)
-  chavy.msg(cookieName, `获取Cookie: 成功`, ``)
+if ($response.body) {
+    $done({
+        body: JSON.stringify({
+            "product_infos": [{
+                "product_id": "5310897792128633390",
+                "start_time": 1417260485,
+                "end_time": 2147483648,
+                "buy_time": "1417260485",
+                "cluster": "offlinedl",
+                "detail_cluster": "offlinedl",
+                "product_name": "gz_telecom_exp"
+            }, {
+                "product_name": "svip2_nd",
+                "product_description": "超级会员",
+                "function_num": 0,
+                "start_time": 1553702399,
+                "buy_description": "",
+                "buy_time": 0,
+                "product_id": "1",
+                "auto_upgrade_to_svip": 0,
+                "end_time": 1672502399,
+                "cluster": "vip",
+                "detail_cluster": "svip",
+                "status": 0
+            }],
+            "currenttime": 1573473597,
+            "reminder": {
+                "reminderWithContent": [],
+                "advertiseContent": []
+            },
+            "request_id": 7501873289383874371
+        })
+    });
+} else {
+    $done({});
 }
-
-function init() {
-  isSurge = () => {
-    return undefined === this.$httpClient ? false : true
-  }
-  isQuanX = () => {
-    return undefined === this.$task ? false : true
-  }
-  getdata = (key) => {
-    if (isSurge()) return $persistentStore.read(key)
-    if (isQuanX()) return $prefs.valueForKey(key)
-  }
-  setdata = (key, val) => {
-    if (isSurge()) return $persistentStore.write(key, val)
-    if (isQuanX()) return $prefs.setValueForKey(key, val)
-  }
-  msg = (title, subtitle, body) => {
-    if (isSurge()) $notification.post(title, subtitle, body)
-    if (isQuanX()) $notify(title, subtitle, body)
-  }
-  log = (message) => console.log(message)
-  get = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.get(url, cb)
-    }
-    if (isQuanX()) {
-      url.method = 'GET'
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
-    }
-  }
-  post = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.post(url, cb)
-    }
-    if (isQuanX()) {
-      url.method = 'POST'
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
-    }
-  }
-  done = (value = {}) => {
-    $done(value)
-  }
-  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
-}
-chavy.done()
