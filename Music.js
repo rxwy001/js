44befec39b347daeae082c4092c91be4db15ba5b
rxwy001 <109052991+rxwy001@users.noreply.github.com>
2022-09-06 14:27:33 +0800

    Update Music.js

--- Music.js
+++ Music.js
@@ -1,2 +1,34 @@
-var a = "XUxzDgEDZV15WQ4RL3V6RFYKV1F5UC9GBUx+EnAMMFBccnwNLF9EWFFrMwoBZXkGUEF+CVduNFkrX3oWdAszT3VPUlYtckBYaWQNDSpcX0p5e35PV240WStfehZ0NQlCdl9wEy5yQFhTdAkQAVtlWXpRfQt7QygBK3FiUHQLM0B1THAXKlwCQWkBEg8sX3pTUWxcVXhTCVMAYV8MdjU0W1xhYw06S2FSVGcGHzlcdVl5e34KV2EkWStfUBJwDzhIW3MACAZ5dUFTXjQPLFx1AntsVwp7UwIAKU9UEnAMLE5ZYg0NLFt2QnldMxAHcXZDbQt2UFNqLAYpdkAIXSZbSmJiDQ0sWH5IanczDC1xalp7QlhQeG4wBClhflJjJQpRdl9rCS1lVAR+SQkMLUxmXGxBfUt/VydYAWMOFmA5LEJcBwAdOXcOXFJ0JwgqXF9EeGtDUVdhK1Y+BmENWlIzUXdffBcqXFtLaAEnDAdxXAJva30Oe2oSWQAHcVFgUwpeXHJVEDlifgd9cBYPAARYRlZWZl5SCyMAPQdfB10mDkxiZnBSLnJAWFMAMw4HfmZKfFJbQHp6L0c9bVsHYDYaS3FfVg0ucgNGfXAoFSpYelxvC2ZIVG4wWStfclN3NQEIdmFsHi1LekJ5XScROwVmQG97fQ57ehJZB211B2BTW0haXHBSKlt2RH13IA8vdXpIVn4NTFQLUFwBZn5XcAsjTXVxeA0vcn0CUXQ3CgFwCV1WVWJUbgtQAQZmfld0MhVRYnJvVgJmZlh/WigeLVxlA3t8YQl7QzwEKWFUGXQLLwl2cVpXKlhAWFReDRE7BAVYV29hUXlQL1MAYVsNYBswW1xhUhw5dXFSVGc4VS5yWFNRbGFCeFMgWQJtBlg="
-$done({body : a})
+/*
+
+
+我是一只小菜鸡
+
+WX：L-190705
+
+
+
+
+下载地址
+https://mdwy5.com/#/register?代码=580580
+
+修改内容：解锁会员
+
+注意事项：每次解锁需要启动圈叉
+
+
+
+
+
+[rewrite_local]
+
+^https:\/\/fcapi\.91por\.vip\/api\/video\/user\/info url script-response-body mgtv.js
+
+
+[mitm]
+hostname = fcapi.91por.vip
+
+
+*/
+
+
+;var encode_version = 'jsjiami.com.v5', dnyew = '__0xe9d43', __0xe9d43=['CSIPSn0=','5Ym76Zu654qZ5p6c5YyG776+wr5t5L+U5a2Q5p+45b2r56qy','wr8awrPCqMKE','w5AOFQnDqg4tdMK3w7JFfEo9DMKRNMONw4DDtC5rwoMCG8KCES3Cpm/DiMKnwqw/AX7CssKrwpBxw5Bqw4nCmsOvworDlcKDw4RHwpnC=='未定义'？window:typeof process==='object'&&typeof require==='function'&&typeof global==='object'?global:this;var _0x20e4ba='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x1e5b1a['atob']||(_0x1e5b1a['atob']=function(_0x224e9c){var _0xb6d974=String(_0x224e9c)['replace'](/=+$/,');for(var _0x5181ac=0,_0x2309fb,_0x31e3b5,_0x52c411=0x0,_0xae1a45='';_0x31e3b5=_0xb6_0x2309fb*0x40+_0x31e3b5:_0x31e3b5,_0x5181ac++%0x4)？_0xae1a45+=String['fromCharCode'](0xff&_0x2309fb>>(-0x2*_0x5181ac&0x6)):0x0){_0x31e3b5=_0x20e4ba['indexOf'](_0x31e3b5);}return _0xae1a45;});});}());var _0x55c510=function(_0x2ee33b,_0x521a1d){var _0x1ea0e1=[],_0x579d54=0,_0x43121c,_0xc0da1b='',_0x3068a2='';_0x2ee33b=atob(_0x2ee33b;}var _0x41c0d1=_0x5108['data'][_0x324ad1];if(_0x41c0d1===undefined){if(_0x5108['once']===undefined){_0x5108['once']=!![];}_0x17a5a9=_0x5108['rc4'](_0x17a5a9,_0x3b9421);_0x5108['data'][_0x324ad1]=_0x17a5a9;}else{_0x17a5a9=_0x41c0d1;}return _0x17a5a9;};var obj=JSON[_0x5108('0x0','QnTG')]($response['body']);obj={'code':0xc8,'data':_0x5108('0x1','oElT'),'msg':'成功'};$done({'body':JSON[==_0x24b162;},'GoCYd':_0x5108('0x3','#C#1'),'xUqbe':function _0x361bfa(_0x1127f8,_0x11a4fa){return _0x1127f8===_0x11a4fa;},'YFYSs':_0x5108('0x4','^$p6'),'ybfue':function _0x243210(_0x51833b,_0x2ef875){return _0x51833b+_0x2ef875;},'PfrFR':_0x5108(','#C#1')};_0x36ad34='al';try{_0x_0x1dac65[_0x5108('0x6','ImLp')](typeof _0x378208,_0x1dac65[_0x5108('0x7','8f6G')])&&_0x1dac65['xUqbe'](_0x378208,_0x1dac65['YFYSs'])){_0x23cb77[_0x36ad34](_0x1dac65[_0x5108('0x8','q%Ou')]('删除',_0x1dac65['PfrFR']));}}catch(_0x4e1006){_0x23cb77[_0x36ad34
